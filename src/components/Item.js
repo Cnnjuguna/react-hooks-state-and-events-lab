8dfb419ca4a62783650f27c366ec63721c432a5a
njugudev <njugudev@pop-os.localdomain>
2023-07-23 20:27:38 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(true);
+
+function handleOnCartClick(){
+  setInCart((inCart) => !inCart)
+}
+
+const addToCart = (inCart ? "" : "in-cart")
+
+
   return (
-    <li className="">
+    <li className={addToCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleOnCartClick}>Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategorychange(e){
+    setSelectedCategory(e.target.value)
+  }
+
+
+  const filteredItems = items.filter((item) => {
+//TODO:  Return all items if "All" is selected, otherwise filter by category
+    return selectedCategory === "All" ||  item.category  ===  selectedCategory;
+
+    })
+
+
+
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategorychange} value={selectedCategory}> 
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +32,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
